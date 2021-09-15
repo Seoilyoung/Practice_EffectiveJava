bbee2dc0ee1af0c3cfe613029578df640fd45491
ilyoung <seo92iyiu@gmail.com>
2021-09-15 10:53:42 +0900

    item2

--- src/item2.java
+++ src/item2.java
@@ -6,7 +6,7 @@ public class item2 {
         private final int calories;
         private final int fat;
         private final int sodium;
-        private final int carbonhydrate;
+        private final int carbohydrate;
 
         public NutritionFacts(int servingSize, int servings){
             this(servingSize, servings);
@@ -24,13 +24,13 @@ public class item2 {
             this(servingSize, servings, calories, fat, sodium);
         }
 
-        public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium, int carbonhydrate){
+        public NutritionFacts(int servingSize, int servings, int calories, int fat, int sodium, int carbohydrate){
             this.servingSize = servingSize;
             this.servings = servings;
             this.calories = calories;
             this.fat = fat;
             this.sodium = sodium;
-            this.carbonhydrate = carbonhydrate;
+            this.carbohydrate = carbohydrate;
         }
     }
 
@@ -43,7 +43,7 @@ public class item2 {
         private int calories = 0;
         private int fat = 0;
         private int sodium = 0;
-        private int carbonhydrate = 0;
+        private int carbohydrate = 0;
 
         public NutritionFacts(){}
         public void setServingSize(int val) { servingSize = val;}
@@ -51,7 +51,7 @@ public class item2 {
         public void setCalories(int val) {calories = val;}
         public void setFat(int val) {fat = val;}
         public void setSodium(int val) {sodium = val;}
-        public void setCarbonhydrate(int val) {carbonhydrate = val;}
+        public void setCarbohydrate(int val) {carbohydrate = val;}
     }
 
     NutritionFacts cocaCola = new NutritionFacts();
@@ -60,5 +60,48 @@ public class item2 {
     cocaCola.setCalories(300);
 
     // 3번쨰 빌더 패턴 Builder pattern
+    public class NutritionFacts{
+        private final int servingSize;
+        private final int servings;
+        private final int calories;
+        private final int fat;
+        private final int sodium;
+        private final int carbohydrate;
 
+        public static class Builder {
+            private final int servingSize;
+            private final int servings;
+
+            private int calories = 0;
+            private int fat = 0;
+            private int sodium = 0;
+            private int carbohydrate = 0;
+
+            public Builder(int servingSize, int servings){
+                this.servingSize = servingSize;
+                this.servings = servings;
+            }
+
+            public Builder calories(int val) { calories = val; return this;}
+            public Builder fat(int val) { fat = val; return this;}
+            public Builder sodium(int val) {sodium = val; return this;}
+            public Builder carbohydrate(int val) {carbohydrate = val; return this;}
+
+            public NutritionFacts build(){
+                return new NutritionFacts(this);
+            }
+        }
+
+        private NutritionFacts(Builder builder){
+            servingSize = builder.servingSize;
+            servings = builder.servings;
+            calories = builder.calories;
+            fat = builder.fat;
+            sodium = builder.sodium;
+            carbohydrate = builder.carbohydrate;
+        }
+    }
+
+    NutritionFacts cocaCola = new NutritionFacts.Builder(100,200)
+            .calories(300).sodium(30).carbohydrate(20).build();
 }
