78e3add1f7ec337c65c2b6f9f336b6b262c23970
ilyoung <seo92iyiu@gmail.com>
2021-09-22 11:41:55 +0900

    item3

---
+++ src/item3.java
@@ -0,0 +1,16 @@
+public class item3 {
+
+/*    public static final item3 INSTANCE = new item3();
+    private item3() {}
+    public void leaveTheBuilding() {}*/
+
+/*    private static final item3 INSTANCE = new item3();
+    private item3() {}
+    public static item3 getInstance() { return INSTANCE;}
+    public void leaveTheBuilding() {}*/
+
+    public static enum Elvis{
+        INSTANCE;
+        public void leaveTheBuilding(){}
+    }
+}
